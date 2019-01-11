513841ebecb32d6159c4407b021cc80de5644316
dubeboy <216004854@student.uj.ac.za>
2019-01-11 20:48:38 +0200

    done

--- app/src/main/java/za/co/dubedivine/taximath/adapter/LedgerAdapter.kt
+++ app/src/main/java/za/co/dubedivine/taximath/adapter/LedgerAdapter.kt
@@ -5,7 +5,6 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TextView
 import za.co.dubedivine.taximath.R
@@ -41,7 +40,7 @@ class LedgerAdapter(private val items: ArrayList<Money>, private val context: Co
 
 
             val animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
-            itemView.startAnimation(animation);
+            itemView.startAnimation(animation)
             itemView.setOnClickListener {
 
             }
--- app/src/main/java/za/co/dubedivine/taximath/ui/main/MainFragment.kt
+++ app/src/main/java/za/co/dubedivine/taximath/ui/main/MainFragment.kt
@@ -1,8 +1,9 @@
 package za.co.dubedivine.taximath.ui.main
 
-import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.support.design.widget.TextInputLayout
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
@@ -10,6 +11,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.main_fragment.*
 import za.co.dubedivine.taximath.R
@@ -23,8 +26,7 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
-
+//    private lateinit var viewModel: MainViewModel
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
@@ -32,7 +34,7 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+//        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
         // get the recycler view first
         // we are using kotlin extensions here so that we dont have to do findviewbyId
@@ -47,54 +49,90 @@ class MainFragment : Fragment() {
         }
 
         fabCalculate.setOnClickListener {
-
-            val amountString = et_amount.text.toString()
-            val numberOfPeopleString = number_of_people.text.toString()
-            val pricePersonInTaxiString = et_taxi_price_person.text.toString()
-
-            tv_layout_taxi_price_per_person.error = null
-            tv_layout_amount.error = null
-            tv_layout_number_of_people.error = null
-
-            if (pricePersonInTaxiString.isBlank()) {
-                Toast.makeText(context, "Enter price per person", Toast.LENGTH_SHORT).show()
-                tv_layout_taxi_price_per_person.error = "Enter price per person"
-                return@setOnClickListener
-            }
-
-            if (amountString.isBlank()) {
-                Toast.makeText(context, "Enter the amount", Toast.LENGTH_SHORT).show()
-                tv_layout_amount.error = "Enter the amount"
-                return@setOnClickListener
-            }
-            if (numberOfPeopleString.isBlank()) {
-                Toast.makeText(context, "Enter the number of people", Toast.LENGTH_SHORT).show()
-                tv_layout_number_of_people.error = "Number of people"
-                return@setOnClickListener
-            }
-
-            val amount = amountString.toDouble()
-            val numberOfPeople = number_of_people.text.toString().toInt()
-            val pricePersonInTaxi = et_taxi_price_person.text.toString().toDouble()
-
-            val priceGivenNumberOfPeople = pricePersonInTaxi * numberOfPeople
-            val change = amount - priceGivenNumberOfPeople
-//            <!--change for given R10 for 2 people | price-->
-
-            tv_status_display.text = getString(R.string.tv_status_display, amount, numberOfPeople, priceGivenNumberOfPeople)
-            if (change < 0) {
-                tv_display.setTextColor(ContextCompat.getColor(context!!, android.R.color.holo_red_light))
-                tv_display.text = getString(R.string.display_text_short, change)
-            } else {
-                tv_display.text = getString(R.string.display_text, change)
-            }
-
-            Log.d(TAG, "the change : $change is given $amount | $numberOfPeople | $pricePersonInTaxi")
-
-            ledgerAdapter.add(Money(numberOfPeople, amount, change, priceGivenNumberOfPeople))
+            calculate(ledgerAdapter)
         }
 
 
+        setViewNextIMEListenerForEditText(et_taxi_price_person, et_amount)
+        setViewNextIMEListenerForEditText(et_amount, et_number_of_people)
+
+        // when the user presses the the calculate this will happen
+        et_number_of_people.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                calculate(ledgerAdapter)
+            }
+            true
+        }
+
+    }
+
+    private fun calculate(ledgerAdapter: LedgerAdapter) {
+        val amountString = et_amount.text.toString()
+        val numberOfPeopleString = et_number_of_people.text.toString()
+        val pricePersonInTaxiString = et_taxi_price_person.text.toString()
+
+        // todo use kotlin lazy
+        clearErrorViewFromEditText(arrayOf(tv_layout_taxi_price_per_person, tv_layout_number_of_people, tv_layout_amount))
+
+
+        if (pricePersonInTaxiString.isBlank()) {
+            Toast.makeText(context, "Enter the price of the taxi per person", Toast.LENGTH_SHORT).show()
+            tv_layout_taxi_price_per_person.error = "Enter the price of the taxi per person"
+            et_taxi_price_person.requestFocus()
+            return
+        }
+
+        if (amountString.isBlank()) {
+            Toast.makeText(context, "Enter the amount that was given", Toast.LENGTH_SHORT).show()
+            tv_layout_amount.error = "Enter amount given"
+            et_amount.requestFocus()
+            return
+        }
+        if (numberOfPeopleString.isBlank()) {
+            Toast.makeText(context, "Enter the number of people", Toast.LENGTH_SHORT).show()
+            tv_layout_number_of_people.error = "Number of people"
+            et_number_of_people.requestFocus()
+            return
+        }
+
+
+
+
+        val amount = amountString.toDouble()
+        val numberOfPeople = et_number_of_people.text.toString().toInt()
+        val pricePersonInTaxi = et_taxi_price_person.text.toString().toDouble()
+
+        val priceGivenNumberOfPeople = pricePersonInTaxi * numberOfPeople
+        val change = amount - priceGivenNumberOfPeople
+        //            <!--change for given R10 for 2 people | price-->
+
+        tv_status_display.text = getString(R.string.tv_status_display, amount, numberOfPeople, priceGivenNumberOfPeople)
+        if (change < 0) {
+            tv_display.setTextColor(ContextCompat.getColor(context!!, android.R.color.holo_red_light))
+            tv_display.text = getString(R.string.display_text_short, change)
+        } else {
+            tv_display.text = getString(R.string.display_text, change)
+        }
+
+        Log.d(TAG, "the change : $change is given $amount | $numberOfPeople | $pricePersonInTaxi")
+
+        ledgerAdapter.add(Money(numberOfPeople, amount, change, priceGivenNumberOfPeople))
+    }
+
+    private fun clearErrorViewFromEditText(view: Array<TextInputLayout>) {
+        view.forEach {
+            it.error = null
+            it.isErrorEnabled = false
+        }
+    }
+
+    private fun setViewNextIMEListenerForEditText(fromEditText: EditText, toEditText: EditText) {
+        fromEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                toEditText.requestFocus()
+            }
+            true
+        }
     }
 
 }
